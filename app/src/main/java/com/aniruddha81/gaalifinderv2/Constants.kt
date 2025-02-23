febc18902e70efd22bad390f6f12ff1cfde49aa3
Aniruddha Roy <ar.roy564@gmail.com>
2025-02-24 02:17:38 +0600

    Amnei

--- app/src/main/java/com/aniruddha81/gaalifinderv2/Constants.kt
+++ app/src/main/java/com/aniruddha81/gaalifinderv2/Constants.kt
@@ -1,6 +1,6 @@
 package com.aniruddha81.gaalifinderv2
 
 object Constants {
-    const val APPWRITE_PROJECT_ID = "67a48eb00005aaeea5e7"
-    const val APPWRITE_BUCKET_ID = "67a493f5003e0d08394a"
+    const val APPWRITE_PROJECT_ID = ""
+    const val APPWRITE_BUCKET_ID = ""
 }
