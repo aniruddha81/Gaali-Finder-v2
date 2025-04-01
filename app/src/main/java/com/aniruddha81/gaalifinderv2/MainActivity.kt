2fccbce07d93125d041b9c944faacf4d8eced53c
Aniruddha Roy <ar.roy564@gmail.com>
2025-04-01 23:12:35 +0600

    Request notification permission for Android 13; refactor permission handling in MainActivity

--- app/src/main/java/com/aniruddha81/gaalifinderv2/MainActivity.kt
+++ app/src/main/java/com/aniruddha81/gaalifinderv2/MainActivity.kt
@@ -3,10 +3,15 @@ package com.aniruddha81.gaalifinderv2
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.app.ActivityCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.aniruddha81.gaalifinderv2.ui.theme.GaaliFinderv2Theme
@@ -16,27 +21,40 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
-                    101
-                )
-            }
-        }
 
         installSplashScreen()
 
         enableEdgeToEdge()
         setContent {
             GaaliFinderv2Theme {
+                RequestNotificationPermission()
                 HomePage()
             }
         }
     }
 }
+
+@Composable
+fun RequestNotificationPermission() {
+    val context = LocalContext.current
+
+    // Notification permission is only required for Android 13 (Tiramisu) and above
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val permission = android.Manifest.permission.POST_NOTIFICATIONS
+
+        val launcher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (!isGranted) {
+                Toast.makeText(context, "Notification permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        LaunchedEffect(Unit) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                launcher.launch(permission)
+            }
+        }
+    }
+}
+
